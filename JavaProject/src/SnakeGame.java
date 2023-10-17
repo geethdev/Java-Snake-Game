eeed26b418123bc101129e33553b8c207d6d465f
Geeth <induwarakwg3573@gmail.com>
2023-10-17 17:52:51 +0530

    Seccond Commit

--- JavaProject/src/SnakeGame.java
+++ JavaProject/src/SnakeGame.java
@@ -5,8 +5,19 @@ import java.util.Random;
 import javax.swing.*;
 
 public class SnakeGame extends JPanel {
+    private class Tile {
+        int x;
+        int y;
+
+        Tile(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
     int boardWidth;
     int boardHeight;
+    int tileSize = 25;
 
     SnakeGame(int boardWidth, int boardHeight) {
         this.boardWidth = boardWidth;
