7291b14a2db376a659fd2fad35f9a38121ef199b
Geeth <induwarakwg3573@gmail.com>
2023-10-18 22:45:59 +0530

    Grids Added

--- SnakeGame/src/SnakeGame.java
+++ SnakeGame/src/SnakeGame.java
@@ -38,9 +38,18 @@ public class SnakeGame extends JPanel {
 
     public void draw(Graphics g) {
 
+        // Grid
+        for (int i = 0; i < boardWidth / tileSize; i++) {
+            // (x1, y1, x2, y2)
+            g.drawLine(i * tileSize, 0, i * tileSize, boardHeight);
+            g.drawLine(0, i * tileSize, boardWidth, i * tileSize);
+
+        }
+
         // snake
         g.setColor(Color.green);
-        g.fillRect(snakeHead.x, snakeHead.y, tileSize, tileSize);
+        g.fillRect(snakeHead.x * tileSize, snakeHead.y * tileSize, tileSize, tileSize);
+
     }
 
 }
